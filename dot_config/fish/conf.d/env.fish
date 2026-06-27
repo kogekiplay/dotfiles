155f0b36b259c39ab0cb3dc442a049fd48e7ac83
lanrhyme <xiao_ren233@foxmail.com>
2026-06-27 22:45:57 +0800

    fix: 修复 fish 环境变量加载脚本语法

--- dot_config/fish/conf.d/env.fish
+++ dot_config/fish/conf.d/env.fish
@@ -1,11 +1,11 @@
 # Load .env file
 if test -f ~/.env
-    while read -l line
-        if string match -qr '^[^#]' -- "$line"
-            set -l parts (string split -m 1 '=' -- "$line")
-            if test (count $parts) -eq 2
+    for line in (cat ~/.env)
+        if not string match -qr '^#' -- "$line"
+            if string match -qr '=' -- "$line"
+                set -l parts (string split -m 1 '=' -- "$line")
                 set -gx $parts[1] (string trim -c '"' -- $parts[2])
             end
         end
-    end < ~/.env
+    end
 end
